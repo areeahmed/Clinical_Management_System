d4416c6a253caec10fba49621587a1d3038d1d2c
Lenovo.co <Lenovo.co@HEAVEN>
2023-02-04 14:48:01 +0300

    test test

--- Clinical_Management_System/Form1.Designer.cs
+++ Clinical_Management_System/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.openEye)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.closeEye)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -56,9 +57,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(147)))), ((int)(((byte)(157)))));
-            this.label1.Location = new System.Drawing.Point(1167, 9);
+            this.label1.Location = new System.Drawing.Point(1556, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 40);
+            this.label1.Size = new System.Drawing.Size(49, 51);
             this.label1.TabIndex = 1;
             this.label1.Text = "X";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -69,9 +71,10 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox1.Location = new System.Drawing.Point(772, 271);
+            this.textBox1.Location = new System.Drawing.Point(1029, 334);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(348, 45);
+            this.textBox1.Size = new System.Drawing.Size(464, 56);
             this.textBox1.TabIndex = 7;
             // 
             // textBox2
@@ -80,9 +83,10 @@
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox2.Location = new System.Drawing.Point(772, 354);
+            this.textBox2.Location = new System.Drawing.Point(1029, 436);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(348, 45);
+            this.textBox2.Size = new System.Drawing.Size(464, 56);
             this.textBox2.TabIndex = 8;
             this.textBox2.UseSystemPasswordChar = true;
             // 
@@ -97,9 +101,10 @@
             "Admin",
             "Doctor",
             "Reciption"});
-            this.comboBox1.Location = new System.Drawing.Point(843, 433);
+            this.comboBox1.Location = new System.Drawing.Point(1124, 533);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(229, 53);
+            this.comboBox1.Size = new System.Drawing.Size(304, 64);
             this.comboBox1.TabIndex = 9;
             this.comboBox1.Text = "Select Role";
             // 
@@ -107,9 +112,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(648, 281);
+            this.label5.Location = new System.Drawing.Point(864, 346);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(98, 26);
+            this.label5.Size = new System.Drawing.Size(127, 33);
             this.label5.TabIndex = 10;
             this.label5.Text = "Username";
             // 
@@ -117,9 +123,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(653, 363);
+            this.label6.Location = new System.Drawing.Point(871, 447);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(93, 26);
+            this.label6.Size = new System.Drawing.Size(119, 33);
             this.label6.TabIndex = 10;
             this.label6.Text = "Password";
             // 
@@ -128,9 +135,10 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(147)))), ((int)(((byte)(157)))));
-            this.label7.Location = new System.Drawing.Point(1114, -14);
+            this.label7.Location = new System.Drawing.Point(1485, -17);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(53, 75);
+            this.label7.Size = new System.Drawing.Size(67, 93);
             this.label7.TabIndex = 1;
             this.label7.Text = "-";
             this.label7.Click += new System.EventHandler(this.label7_Click);
@@ -141,9 +149,10 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button2.Location = new System.Drawing.Point(843, 537);
+            this.button2.Location = new System.Drawing.Point(1124, 661);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(229, 68);
+            this.button2.Size = new System.Drawing.Size(305, 84);
             this.button2.TabIndex = 11;
             this.button2.Text = "Login";
             this.button2.UseVisualStyleBackColor = false;
@@ -152,9 +161,10 @@
             // 
             this.openEye.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(147)))), ((int)(((byte)(157)))));
             this.openEye.Image = global::Clinical_Management_System.Properties.Resources._2072001;
-            this.openEye.Location = new System.Drawing.Point(1038, 354);
+            this.openEye.Location = new System.Drawing.Point(1384, 436);
+            this.openEye.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.openEye.Name = "openEye";
-            this.openEye.Size = new System.Drawing.Size(82, 45);
+            this.openEye.Size = new System.Drawing.Size(109, 55);
             this.openEye.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.openEye.TabIndex = 12;
             this.openEye.TabStop = false;
@@ -164,9 +174,10 @@
             // 
             this.closeEye.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(147)))), ((int)(((byte)(157)))));
             this.closeEye.Image = global::Clinical_Management_System.Properties.Resources._2072002;
-            this.closeEye.Location = new System.Drawing.Point(1038, 354);
+            this.closeEye.Location = new System.Drawing.Point(1384, 436);
+            this.closeEye.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.closeEye.Name = "closeEye";
-            this.closeEye.Size = new System.Drawing.Size(82, 45);
+            this.closeEye.Size = new System.Drawing.Size(109, 55);
             this.closeEye.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.closeEye.TabIndex = 12;
             this.closeEye.TabStop = false;
@@ -175,9 +186,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Clinical_Management_System.Properties.Resources.Vector;
-            this.pictureBox2.Location = new System.Drawing.Point(878, 57);
+            this.pictureBox2.Location = new System.Drawing.Point(1171, 70);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(131, 137);
+            this.pictureBox2.Size = new System.Drawing.Size(175, 169);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
@@ -190,17 +202,19 @@
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(-4, 1);
+            this.panel1.Location = new System.Drawing.Point(-5, 1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(604, 710);
+            this.panel1.Size = new System.Drawing.Size(805, 874);
             this.panel1.TabIndex = 3;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Clinical_Management_System.Properties.Resources.ICON_6;
-            this.pictureBox1.Location = new System.Drawing.Point(206, 121);
+            this.pictureBox1.Location = new System.Drawing.Point(275, 149);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(168, 168);
+            this.pictureBox1.Size = new System.Drawing.Size(224, 207);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -212,9 +226,10 @@
             this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label4.Font = new System.Drawing.Font("Calibri", 40F);
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(195, 388);
+            this.label4.Location = new System.Drawing.Point(260, 478);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(182, 66);
+            this.label4.Size = new System.Drawing.Size(227, 82);
             this.label4.TabIndex = 4;
             this.label4.Text = "System";
             // 
@@ -225,9 +240,10 @@
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label3.Font = new System.Drawing.Font("Calibri", 40F);
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(195, 322);
+            this.label3.Location = new System.Drawing.Point(260, 396);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(321, 66);
+            this.label3.Size = new System.Drawing.Size(398, 82);
             this.label3.TabIndex = 3;
             this.label3.Text = "Management";
             // 
@@ -238,18 +254,30 @@
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label2.Font = new System.Drawing.Font("Calibri", 40F);
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(214)))), ((int)(((byte)(214)))));
-            this.label2.Location = new System.Drawing.Point(58, 322);
+            this.label2.Location = new System.Drawing.Point(77, 396);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(144, 66);
+            this.label2.Size = new System.Drawing.Size(179, 82);
             this.label2.TabIndex = 2;
             this.label2.Text = "Clinic";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(920, 179);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1217, 702);
+            this.ClientSize = new System.Drawing.Size(1623, 864);
             this.ControlBox = false;
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.closeEye);
             this.Controls.Add(this.openEye);
             this.Controls.Add(this.button2);
@@ -263,6 +291,7 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
@@ -294,6 +323,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox closeEye;
         private System.Windows.Forms.PictureBox openEye;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
